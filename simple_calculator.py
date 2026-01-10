9dce8b96d84958ab2f750f917796d8edd0b79abb
CYRIL P JOSE <cyrilpjose2025@gmail.com>
2026-01-10 14:09:54 +0530

    created simple_calculator.py file

---
+++ simple_calculator.py
@@ -0,0 +1,18 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+print("OPTIONS\n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Reminder")
+while True:
+    c=int(input("Enter your choice:"))
+    if c==1:
+        print(a ,"+", b ,"=", a+b)
+    elif c==2:
+        print(a ,"-", b ,"=", a-b)
+    elif c==3:
+        print(a ,"*", b ,"=", a*b)
+    elif c==4:
+        print(a,"/",b ,"=", a/b)
+    elif c==5:
+        print("Reminder of", a,"/",b ,"=", a%b)
+    else:
+        print("Invalid choice")
+        break
