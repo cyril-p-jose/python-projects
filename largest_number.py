21653b04cfc354938463fa954fdab838f28b3b49
CYRIL P JOSE <cyrilpjose2025@gmail.com>
2026-01-11 21:19:53 +0530

    Update largest_number.py

---
+++ largest_number.py
@@ -0,0 +1,9 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+
+if a>b:
+    print(f"largest number is {a}")
+elif a<b:
+    print(f"largest number is {b}")
+else:
+    print(f"Two numbers are equal")
