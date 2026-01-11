f575f397dffa88f011846f08d276ab9e0419caca
CYRIL P JOSE <cyrilpjose2025@gmail.com>
2026-01-11 21:34:10 +0530

    Add comment for temperature conversion program
    Added a comment to explain the purpose of the program.

--- temperature_converter.py
+++ temperature_converter.py
@@ -1,3 +1,4 @@
+#Python programme to convert temperature in celsius scale to fahrenheit scale
 celsius=float(input("Enter temperature in Celsius:"))
 fahrenheit=(celsius*9/5)+32
 print("Temperature in Fahrenheit:",fahrenheit )  
