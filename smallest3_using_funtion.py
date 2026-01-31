6973aaff6a39a33e8b8ea031f4cbf51f2fe59500
CYRIL P JOSE <cyrilpjose2025@gmail.com>
2026-01-31 13:48:04 +0530

    Create smallest3_using_funtion.py

---
+++ smallest3_using_funtion.py
@@ -0,0 +1,8 @@
+def smallest_of_three(a, b, c):
+    return min(a, b, c)
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+print("Smallest number is:", smallest_of_three(a, b, c))
